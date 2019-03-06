b7cb0347d37a960dc4b44a8a3ba63b86ca777662
Corentin Jacques <cjacques@student.42.fr>
2019-03-06 17:14:22 +0100

    clean

--- scrape.js
+++ scrape.js
@@ -1,86 +1,98 @@
 #!/usr/bin/env node
 
-const Nightmare = require('nightmare')
-const argv = require('yargs').argv
+const Nightmare = require('nightmare');
+const argv = require('yargs').argv;
+const cheerio = require('cheerio');
 const chalk = require('chalk');
 const async = require('async');
 const fs = require('fs');
-var sanitizeHtml = require('sanitize-html');
+const sanitizeHtml = require('sanitize-html');
 const uuidv1 = require('uuid/v1');
 
 let directory = 'file';
-let { _, i, h, f } = argv;
+let { _: urls } = argv;
 
-let option = {
-  'i': 5,
-}
-option.i = (i) ? i : option.i;
+let options = {
+  h: argv.h,
+  i: argv.i || 5,
+  f: argv.f,
+  length: urls.length
+};
 
-if (f && _.length === 0) {
-  let data = fs.readFileSync( `${ process.env.PWD }/${f}`, {
-    encoding: 'utf8'
-  });
-  if (data) {
-    _ = data.split('\n');
-  }
-}
-
-let urls = _.slice();
-let list = _;
-let len = list.length;
-
-if (_.length < option.i) {
-  option.i = 1;
-}
-
-if (h) {
-  console.log(chalk.yellow(`Usage: scrape <url> <url> <url> ...\n`));
-  console.log(chalk.yellow(`\tscrape https://google.com https://exemple.com http://localhost:3000`));
-  console.log(chalk.yellow(`\tscrape -i 6 https://google.com\t// speed of 6 request per second, default: 5`));
-  console.log(chalk.yellow(`\tscrape -f file.txt\t// One url per line in the file`));
-  return;
-}
-
-if (urls.length === 0) {
-  console.log(chalk.yellow(`scrape -h`));
-  return;
-}
-
-console.log(chalk.green('Loading ..\n'));
-popAndSaveUrls();
-var errors = [];
+let errors = [];
 let q = async.queue(function(obj, callback) {
   request(obj, function(err, res) {
     if (err) {
       errors.push(obj);
       console.log(chalk.red(`${obj} KO`));
-      popAndSaveUrls(obj, callback);
-      return;
+      return popAndSaveUrls(obj, callback);
     }
-    var clean = sanitizeHtml(res);
+
+    // const $ = cheerio.load(res);
+    // const arr = obj.split('/');
+    // const names = obj.split('/lbm');
+    // let name;
+    // if (names[1].startsWith('-')) {
+    //   name = names[1].substring(1).split('.')[0];
+    // }
+    // let city;
+    // arr.forEach((elmt, i) => {
+    //   if (elmt === 'laboratoires') {
+    //     city = arr[i + 1];
+    //   }
+    // })
+    // let selector = '#main-content > div > div:nth-child(2) > div > div.laboratory_mainBlock.col-sm-12 > div > div > ul > li';
+    // let services = [];
+    // let base = [obj, city, name];
+    // $(selector).each(function(i, elem) {
+    //   let service = $(this).text().trim();
+    //   services.push(service);
+    // });
+    // let final = base.concat(services)
+    // console.log(res);
+    save(res);
     console.log(chalk.green(`${obj} OK`));
-    save(clean);
     popAndSaveUrls(obj, callback);
   });
-  //   headers : {
-  //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'
-  //   }
-}, option.i);
+}, options.i);
 
 q.drain = function() {
-  let txt = errors.join('\n');
-  let fileError = 'error.txt';
-  if (txt !== '') {
-    fs.writeFileSync(fileError, txt);
-    console.log(chalk.yellow('Check error.txt to see problematic url'));
+  let file = 'error.txt';
+  if (errors.length) {
+    let txt = errors.join('\n');
+    try {
+      fs.writeFileSync(file, txt);
+      console.log(chalk.yellow('Check error.txt to see problematic url'));
+    } catch (e) {
+      console.log(chalk.yellow(`File: "${file}" can't be created/writed ...\n`));
+    }
   }
   console.log();
-  console.log(chalk.yellow(`urls ${list.length - errors.length}/${list.length}`));
-  console.log(chalk.red(`errors ${errors.length}`));
-  console.log(chalk.yellow('Finished !'));
+  console.log(chalk.yellow(`urls ${options.length - errors.length}/${options.length}`), chalk.red(`errors ${errors.length}/${options.length}`));
 };
 
-q.push(list);
+if (options.f && !options.length) {
+  try {
+    let data = fs.readFileSync( `${ process.env.PWD }/${options.f}`, { encoding: 'utf8' });
+    urls = data.split('\n').filter((el) => el);
+    options.length = urls.length;
+  } catch (e) {
+    console.log(chalk.yellow(`File: "${options.f}" doesn't exists ...\n`));
+  }
+}
+
+if (options.h) {
+  console.log(chalk.yellow(`Usage: scrape <url> <url> <url> ...\n`));
+  console.log(chalk.yellow(`\tscrape https://google.com https://exemple.com http://localhost:3000`));
+  console.log(chalk.yellow(`\tscrape -i 6 https://google.com\t// speed of 6 request per second, default: 5`));
+  console.log(chalk.yellow(`\tscrape -f file.txt\t// One url per line in the file`));
+} else if (!urls.length) {
+  console.log(chalk.yellow(`scrape -h`));
+} else {
+  console.log(chalk.blue('Loading ..\n'));
+  popAndSaveUrls();
+  q.push(urls);
+}
 
 function popAndSaveUrls(obj, callback) {
   if (obj) {
@@ -90,36 +102,32 @@ function popAndSaveUrls(obj, callback) {
     }
   }
   if (urls.length === 0) {
-    console.log();
     fs.unlink('urls.txt', (err) => {
-      console.log(chalk.green('urls.txt has been deleted'));
-      if (callback) {
-        callback();
-      }
+      console.log();
+      console.log(chalk.blue('urls.txt has been deleted'));
+      callback && callback();
     });
   } else {
     fs.writeFileSync('urls.txt', urls.join('\n'));
-    if (callback) {
-      callback();
-    }
+    callback && callback();
   }
 }
 
 function save(html) {
-  let randomFilename = uuidv1().split('-').join('');
-  if ( !fs.existsSync( `${directory}` ) )
-			fs.mkdirSync( `${directory}` );
-	fs.writeFileSync( `${directory}/${randomFilename}.html`, html);
+  var withoutTags = sanitizeHtml(html);
+  let uuid = uuidv1().split('-').join('');
+  if ( !fs.existsSync( `${directory}` ) ) {
+    fs.mkdirSync( `${directory}` );
+  }
+	fs.writeFileSync( `${directory}/${uuid}.html`, withoutTags);
 }
 
 function request(url, callback) {
-  const nightmare = Nightmare({ show: false })
+  const nightmare = Nightmare({ show: false });
   nightmare
     .goto(url)
     .wait(2000)
-    .evaluate(function() {
-      return document.body.innerHTML
-    })
+    .evaluate(() => document.body.innerHTML)
     .end()
     .then(function(obj) {
       callback(null, obj);
